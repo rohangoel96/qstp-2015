0b15cb261ecb21460134f07e1837e273d32db0a9
Rohan Goel <rohangoel0296@gmail.com>
2015-05-28 22:25:23 +0530

    Python Week1

---
+++ Training/Python/1/Assignment1_rohangoel96.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+'''
+Problem 1.
+Write a program that does the following in order:
+1. Asks the user to enter his/her last name.
+2. Asks the user to enter his/her first name.
+3. Prints out the user’s first and last names in that order.
+
+An example of an interaction with your program is shown below (the words with a single star
+are from the computer, based on your commands, the words with a dollar are a user’s input –
+
+$Enter your last name:
+**Anmol
+$Enter your first name:
+**Sahoo
+$Anmol
+$Sahoo
+
+(Yeah, I am a little self obssessed - Anmol)
+
+'''
+
+
+
+
+l_name=raw_input("Enter your last name: ");
+f_name=raw_input("Enter your first name: ");
+
+print l_name
+print f_name
