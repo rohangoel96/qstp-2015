be8e32e4f9fb063e522d5d07aa425dd7859776f0
Mohit Shah <shahmohit96@gmail.com>
2015-06-01 17:39:04 +0530

    COmmit

--- Training/Python/Assignment2_Mohit.py
+++ Training/Python/Assignment2_Mohit.py
@@ -4,8 +4,8 @@ year = raw_input("Enter a year:")
 print date + "/" + month + "/" + year
 move_ahead = raw_input("No. of months to move ahead")
 if int(month) + int(move_ahead) > 12:
-   year = int(year) + 1
-   month = int(month) + int(move_ahead) - 12
-else:
-   month = int(month) + int(move_ahead)
+     year = int(year) + 1
+     month = int(month) + int(move_ahead) - 12
+else: 
+     month = int(month) + int(move_ahead)
 print date + "/" + str(month) + "/" + str(year)
