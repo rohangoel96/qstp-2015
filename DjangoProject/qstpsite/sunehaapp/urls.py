d6ade90e2cafb60a33f41a5ac4bf6a26b5ae5b24
Anmol Sahoo <anmol.sahoo25@gmail.com>
2015-06-30 17:12:45 +0000

    30th Commit 2

--- DjangoProject/qstpsite/sunehaapp/urls.py
+++ DjangoProject/qstpsite/sunehaapp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from sunehaapp import views
+
 urlpatterns = [
     url(r'^homepage/',views.HomePage,name = "Home Page"),
+    url(r'^page1/',views.Page1,name = "Page 1"),
 ]
