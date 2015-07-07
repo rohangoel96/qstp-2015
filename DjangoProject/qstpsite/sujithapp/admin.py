5e1433acf1154354766c75ce984751173733471a
sujithpai96 <sujithpai96@yahoo.com>
2015-07-07 23:00:07 +0530

    7 July - 1

--- DjangoProject/qstpsite/sujithapp/admin.py
+++ DjangoProject/qstpsite/sujithapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from sujithapp.models import word
 # Register your models here.
+admin.site.register(word)
--- DjangoProject/qstpsite/sujithapp/models.py
+++ DjangoProject/qstpsite/sujithapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class word(models.Model):
+	word_text = models.CharField(max_length = 250)
+	word_language = models.Charfield(max_length = 250)
+	
+	def __unicode__(self):
+		return self.word_text
