b8c1cfdf7252f4ace8a16d9b0d33be4e58b61b44
sujithpai96 <sujithpai96@yahoo.com>
2015-07-07 23:58:57 +0530

    7 July - 2

--- DjangoProject/qstpsite/sujithapp/models.py
+++ DjangoProject/qstpsite/sujithapp/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-
+from sunehaapp.models import language
 # Create your models here.
 class word(models.Model):
 	word_text = models.CharField(max_length = 250)
-	word_language = models.Charfield(max_length = 250)
+	word_meaning = models.CharField(max_length = 250)
+	language = models.ForeignKey(language)
+	figureofspeech = models.ForeignKey(figureofspeech)
 	
 	def __unicode__(self):
 		return self.word_text
+		
+class figureofspeech(models.Model):
+	figureofspeech_text = models.CharField(max_length = 250)
+	
