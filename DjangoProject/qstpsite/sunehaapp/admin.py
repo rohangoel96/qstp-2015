b77fb94f1355f13f7d5fa632c0f7007d793c0b22
SunehaBagri <sunehabagri@gmail.com>
2015-07-08 00:01:36 +0530

    randomcommit

--- DjangoProject/qstpsite/sunehaapp/admin.py
+++ DjangoProject/qstpsite/sunehaapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from sunehaapp.models import word
+admin.site.register(word)
 # Register your models here.
--- DjangoProject/qstpsite/sunehaapp/models.py
+++ DjangoProject/qstpsite/sunehaapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from sujithapp.models import figureofspeech
+ 
+class word(models.Model):
+        
+        language = models.ForeignKey(language)
+        figureofspeech = models.ForeignKey(figureofspeech)
+        word_text = models.CharField(max_length = 250)
+        word_meaning = models.CharField(max_length = 300)
+ 
+        def __unicode__(self):
+                return self.word_text
+class language(newmodel.Model):
+        language_Country = newmodels.CharField(max_lenth = 300)
 # Create your models here.
