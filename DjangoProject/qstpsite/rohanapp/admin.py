2c976e9c18a84ec98d4e4eed6ca8252ed7fc7356
Anmol Sahoo <anmol.sahoo25@gmail.com>
2015-07-09 23:45:37 +0530

    final for 9th July

--- DjangoProject/qstpsite/rohanapp/admin.py
+++ DjangoProject/qstpsite/rohanapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from rohanapp.models import LoginProfile
 
+admin.site.register(LoginProfile)
 # Register your models here.
--- DjangoProject/qstpsite/rohanapp/views.py
+++ DjangoProject/qstpsite/rohanapp/views.py
@@ -3,26 +3,26 @@ from django.http import HttpResponse
 
 from rohanapp.models import LoginProfile
 
-def Login(request):                                                                                                   
-	username = request.POST['username']                                                                           
-	password = request.POST['password']                                                                           
-	login=""
+def Login(request): 
+	login = ""
+	if request.POST:
+		username = request.POST['username']                                                                           
+		password = request.POST['password']                                                                           
+		utemp = LoginProfile.objects.get(username = username) 
 
-	utemp = LoginProfile.objects.get(username = username) 
-
-	if utemp is not None:
-		if utemp.password == password:
-			login = "Success"
-	else:
-		login = "User not found"
-
-	context = {'login': login}
-
+		if utemp is not None:
+			if utemp.password == password:
+				login = "Success"
+			else:
+				login = "Wrong Password"
 	
-	return render(request,"rohan_templates/login.html",context)                                                                 
+	context = {'login': login}
+	return render(request,"rohan/login.html",context)                                                                 
 	       
+		   
+		   
 def Page1(request):                                                                                                   
-	return render(request,"rohan_templates/page1.html")                                                                    
+	return render(request,"rohan/page1.html")                                                                    
 
 def Page2(request):                                                                                                   
-	return render(request,"rohan_templates/page2.html") 
+	return render(request,"rohan/page2.html") 
